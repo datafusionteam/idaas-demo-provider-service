ec57c29598a015da9058c82ebc0fc1f3cee7da8d
samgamage <sammygamage@gmail.com>
2022-03-24 15:46:38 -0400

    chore: Update copyright headers

--- src/fhir/server.js
+++ src/fhir/server.js
@@ -1,13 +1,28 @@
 /*
  * (C) Copyright Data Fusion Specialists. 2022
  *
- * SPDX-License-Identifier: Apache-2.0
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 const { Axios } = require("axios");
 
 class FHIRServer {
-  constructor(url, basePath = "/hapi-fhir-jpaserver/fhir", version = "R4") {
+  constructor(
+    url,
+    basePath = "/hapi-fhir-jpaserver/fhir",
+    version = "R4",
+    debug = false
+  ) {
     this.config = {
       url,
       basePath,
@@ -15,6 +30,7 @@ class FHIRServer {
     };
     this.client = new Axios({
       baseURL: `${url}${this.config.basePath}`,
+      validateStatus: (status) => status < 500,
     });
   }
 
